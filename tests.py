f0b7e4fd4d8171330fa95ecb1ebe1b60624a8dd2
kyleecodes <kfieldsinmd@gmail.com>
2021-05-20 21:30:41 -0400

    Wrote tests for request_artist_info & request_song_url

--- tests.py
+++ tests.py
@@ -1,5 +1,18 @@
+from main import request_artist_info, request_song_url
 
 
 # artist info test
+def test_request_artist_info():
+    response = request_artist_info('ariana grande', 2)
+    assert response.status_code == 200
+
 
 # song url test
+def test_request_song_url():
+    url = request_song_url('ariana grande', 1)
+    assert url == ['https://genius.com/Ariana-grande-thank-u-next-lyrics']
+
+
+if __name__ == '__main__':
+    test_request_artist_info()
+    test_request_song_url()
